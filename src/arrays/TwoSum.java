31c043a013026f4454fa5fdf49d9cb55e6e3fcf9
jpnsingh <jpnsingh86@gmail.com>
2022-07-08 01:43:21 +0530

    Arrays - Two sum;

--- src/arrays/TwoSum.java
+++ src/arrays/TwoSum.java
@@ -29,10 +29,10 @@ public class TwoSum {
     public static void main(String[] args) {
 //        int[] nums = {2, 7, 11, 15};
 //        int[] nums = {1, 2, 7, 11, 15};
-//        int[] nums = {1, 2, 5, 7, 11, 15};
-//        int target = 9;
-        int target = 6;
-        int[] nums = {3, 2, 4};
+        int[] nums = {1, 5, 7, 11, 2, 15};
+        int target = 9;
+//        int target = 6;
+//        int[] nums = {3, 2, 4};
         System.out.println(Arrays.toString(twoSum(nums, target)));
     }
 }
